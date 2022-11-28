faacc8a3997a73b5372beb98e28a8fc243bce93d
AlexFerMar <afernandezmartinez@danielcastelao.org>
2022-11-28 09:39:18 +0100

    Implementada Base de datos

--- app/src/main/java/com/example/simondice/view/MainActivity.kt
+++ app/src/main/java/com/example/simondice/view/MainActivity.kt
@@ -11,7 +11,6 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.viewModels
 
-
 import androidx.appcompat.app.AppCompatActivity
 import com.example.simondice.R
 
